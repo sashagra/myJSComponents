9ad11472cdef761796f21e694189986489c56329
Alex G <archon.san@gmail.com>
2020-03-20 22:36:55 +0300

    added modal options

--- my-modal/index.js
+++ my-modal/index.js
@@ -1,1 +1,9 @@
-const myModal = Q.modal()
+const myModal = Q.modal({
+    title: "My Modal Super Puper",
+    closable: true,
+    elName: 'myModal',
+    closeClasses: ['close', 'overlay', 'ok', 'cancel'], // {modal-var}
+    content: 'I want to tell you about... some words'
+})
+
+// myModal.open();
--- my-modal/plugins/modal.js
+++ my-modal/plugins/modal.js
@@ -1,11 +1,11 @@
 // https://www.youtube.com/watch?v=FX2fiUvrYP4
 
-// title: String
-// closable: Boolean
+// title: String ---- DONE
+// closable: Boolean --- DONE
 // content: String
 // width: String ('100px')
 // destroy(): void
-// Окно должно закрываться
+// Окно должно закрываться --- DONE
 // -----------------------
 // setContent(html: String): void PUPLIC
 // onClose(): void
@@ -18,20 +18,24 @@
 function __createModal(options) {
         const modal = document.createElement('div')
         modal.classList.add('vmodal')
+        let disabled, closeBtn = ""; 
+        if (options.closable)  {
+            closeBtn = '<span class="modal-close">&times;</span>';
+        } else {disabled = 'disabled'}
         modal.insertAdjacentHTML('afterbegin', `
             <div class="modal-overlay">
                 <div class="modal-window">
                     <div class="modal-header">
-                        <span class="modal-title">Modal Title</span>
-                        <span onClick="myModal.close()" class="modal-close">&times;</span>
+                        <span class="modal-title">${options.title}</span>
+                        ${closeBtn}
                     </div>
                     <div class="modal-body">
                         <p>Lorem ipsum dolor sit.</p>
                         <p>Lorem ipsum dolor sit.</p>
                     </div>
                     <div class="modal-footer">
-                        <button onClick="myModal.close()">OK</button>
-                        <button onClick="myModal.close()">Cancel</button>
+                        <button class="modal-ok" ${disabled}>OK</button>
+                        <button class="modal-cancel" ${disabled}>Cancel</button>
                     </div>
                 </div>
             </div>
@@ -43,21 +47,35 @@ function __createModal(options) {
 Q.modal = function(options) {
     const ANIMATION_SPEED = 200
     const $modal = __createModal(options )
+    $modal.querySelector('.modal-body').textContent = options.content
     let closing = false
 
     return {
         open() {
-            !closing && $modal.classList.add('open')
-        },
-        close() {
-            closing = true
-            $modal.classList.remove('open')
-            $modal.classList.add('hide')
-            setTimeout(() => {
-                $modal.classList.remove('hide')
-                closing = false
-            }, ANIMATION_SPEED);
+            !closing && $modal.classList.add('open');
+            document.body.addEventListener('click', this.close);
+                
 
+        },
+        close(e) {
+            
+            if (e.target.classList[0]) {    
+                const target = e.target.classList[0].split('-')[1]
+                if (options.closeClasses.indexOf(target) !== -1) {
+            
+                    if (options.closable) {
+                        closing = true
+                        $modal.classList.remove('open')
+                        $modal.classList.add('hide')
+
+                        setTimeout(() => {
+                            $modal.classList.remove('hide')
+                            closing = false;
+                        }, ANIMATION_SPEED);
+                        document.body.removeEventListener('click', close);
+                        }
+            }
+        }
         },
         destroy() {}
     }
